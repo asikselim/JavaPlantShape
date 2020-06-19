15d4db3afabe7f338650f58d99a268173f31e729
Selim Aşık <selim.asik@hotmail.com>
2020-06-19 11:21:35 +0300

    Add files via upload

---
+++ H5180054/src/com/company/Main.java
@@ -0,0 +1,694 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AllMethod();
+    }
+
+    public static void AllMethod(){
+        FirstMethod();   //While Loop
+        SecondMethod();  //Nested Loop
+        ThirdMethod();   //Nested Loop
+        FourthMethod();
+        FifthMethod();
+        SixthMethod();
+        SeventhMethod();
+        EighthMethod();
+        NinthMethod();
+        TenthMethod();
+        EleventhMethod();
+        TwelfthMethod();
+
+    }
+
+
+
+    public static void FirstMethod(){
+        int i = 0;
+        while( i<=17)
+        {
+            if(i==17){
+                System.out.println("|");
+            }
+            else{
+                System.out.print(" ");
+            }
+            i++;
+        }
+
+    }
+
+
+    public static void SecondMethod(){
+        for(int j=1;j<=2; j++){
+            for(int i=0;i<=18; i++)
+            {
+                if(i==16){
+                    System.out.print("|");
+                }
+                else if(i==17){
+                    System.out.print(".");
+                }
+                else if(i==18){
+                    System.out.println("|");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+
+        }
+
+    }
+
+
+    public static void ThirdMethod(){
+        for(int j=1;j<=3; j++){
+            for(int i=0; i<=19; i++)
+            {
+                if(i==15){
+                    System.out.print("|");
+                }
+                else if(i==16){
+                    System.out.print("\\");
+                }
+                else if(i==17){
+                    System.out.print(".");
+                }
+                else if(i==18){
+                    System.out.print("/");
+                }
+                else if(i==19){
+                    System.out.println("|");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+        }
+
+    }
+
+    public static void FourthMethod(){
+        for(int i=0; i<=33;i++)
+        {
+            if(i==1){
+                System.out.print("\\");
+            }
+            else if(i==2){
+                System.out.print("-");
+            }
+            else if(i==3){
+                System.out.print(".");
+            }
+            else if(i==4){
+                System.out.print(".");
+            }
+            else if(i==5){
+                System.out.print(".");
+            }
+            else if(i==6){
+                System.out.print(".");
+            }
+            else if(i==7){
+                System.out.print("\\");
+            }
+            else if(i==14){
+                System.out.print("|");
+            }
+            else if(i==15){
+                System.out.print("\\");
+            }
+            else if(i==16){
+                System.out.print("\\");
+            }
+            else if(i==17){
+                System.out.print(".");
+            }
+            else if(i==18){
+                System.out.print("/");
+            }
+            else if(i==19){
+                System.out.print("/");
+            }
+            else if(i==20){
+                System.out.print("|");
+            }
+            else if(i==27){
+                System.out.print("/");
+            }
+            else if(i==28){
+                System.out.print(".");
+            }
+            else if(i==29){
+                System.out.print(".");
+            }
+            else if(i==30){
+                System.out.print(".");
+            }
+            else if(i==31){
+                System.out.print(".");
+            }
+            else if(i==32){
+                System.out.print("-");
+            }
+            else if(i==33){
+                System.out.println("/");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static void FifthMethod(){
+
+
+        for(int j =0;j<=32;j++){
+            if(j==2){
+                System.out.print("\\");
+            }
+            else if(j==3){
+                System.out.print("-");
+            }
+            else if(j==4){
+                System.out.print("-");
+            }
+            else if(j==5){
+                System.out.print("-");
+            }
+            else if(j==6){
+                System.out.print("-");
+            }
+            else if(j==7){
+                System.out.print(".");
+            }
+            else if(j==8){
+                System.out.print("\\");
+            }
+            else if(j==14){
+                System.out.print("|");
+            }
+            else if(j==15){
+                System.out.print("\\");
+            }
+            else if(j==16){
+                System.out.print("\\");
+            }
+            else if(j==17){
+                System.out.print(".");
+            }
+            else if(j==18){
+                System.out.print("/");
+            }
+            else if(j==19){
+                System.out.print("/");
+            }
+            else if(j==20){
+                System.out.print("|");
+            }
+            else if(j==26){
+                System.out.print("/");
+            }
+            else if(j==27){
+                System.out.print(".");
+            }
+            else if(j==28){
+                System.out.print("-");
+            }
+            else if(j==29){
+                System.out.print("-");
+            }
+            else if(j==30){
+                System.out.print("-");
+            }
+            else if(j==31){
+                System.out.print("-");
+            }
+            else if(j==32){
+                System.out.println("/");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static void SixthMethod(){
+        for(int j =1;j<=32;j++){
+            if(j==3){
+                System.out.print(" ");
+                System.out.print("\\");
+            }
+            else if(j==4){
+                System.out.print("-");
+            }
+            else if(j==5){
+                System.out.print("-");
+            }
+            else if(j==6){
+                System.out.print("-");
+            }
+            else if(j==7){
+                System.out.print("-");
+            }
+            else if(j==8){
+                System.out.print(".");
+            }
+            else if(j==9){
+                System.out.print("\\");
+            }
+            else if(j==14){
+                System.out.print("|");
+            }
+            else if(j==15){
+                System.out.print("\\");
+            }
+            else if(j==16){
+                System.out.print("\\");
+            }
+            else if(j==17){
+                System.out.print(".");
+            }
+            else if(j==18){
+                System.out.print("/");
+            }
+            else if(j==19){
+                System.out.print("/");
+            }
+            else if(j==20){
+                System.out.print("|");
+            }
+            else if(j==25){
+                System.out.print("/");
+            }
+            else if(j==26){
+                System.out.print(".");
+            }
+            else if(j==27){
+                System.out.print("-");
+            }
+            else if(j==28){
+                System.out.print("-");
+            }
+            else if(j==29){
+                System.out.print("-");
+            }
+            else if(j==30){
+                System.out.print("-");
+            }
+            else if(j==31){
+                System.out.println("/");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static void SeventhMethod(){
+        for(int j =1;j<=30;j++){
+            if(j==4){
+                System.out.print("\\");
+            }
+            else if(j==5){
+                System.out.print("-");
+            }
+            else if(j==6){
+                System.out.print(".");
+            }
+            else if(j==7){
+                System.out.print(".");
+            }
+            else if(j==8){
+                System.out.print(".");
+            }
+            else if(j==9){
+                System.out.print(".");
+            }
+            else if(j==10){
+                System.out.print("\\");
+            }
+            else if(j==15){
+                System.out.print("|");
+            }
+            else if(j==16){
+                System.out.print("\\");
+            }
+            else if(j==17){
+                System.out.print(".");
+            }
+            else if(j==18){
+                System.out.print("/");
+            }
+            else if(j==19){
+                System.out.print("|");
+            }
+            else if(j==24){
+                System.out.print("/");
+            }
+            else if(j==25){
+                System.out.print(".");
+            }
+            else if(j==26){
+                System.out.print(".");
+            }
+            else if(j==27){
+                System.out.print(".");
+            }
+            else if(j==28){
+                System.out.print(".");
+            }
+            else if(j==29){
+                System.out.print("-");
+            }
+            else if(j==30){
+                System.out.println("/");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static void EighthMethod(){
+        for(int j =1;j<=30;j++){
+            if(j==6){
+                System.out.print("\\");
+            }
+            else if(j==7){
+                System.out.print(".");
+            }
+            else if(j==8){
+                System.out.print(".");
+            }
+            else if(j==9){
+                System.out.print(".");
+            }
+            else if(j==10){
+                System.out.print(".");
+            }
+            else if(j==11){
+                System.out.print(".");
+            }
+            else if(j==12){
+                System.out.print("\\");
+            }
+            else if(j==16){
+                System.out.print("|");
+            }
+            else if(j==17){
+                System.out.print("\\");
+            }
+            else if(j==18){
+                System.out.print(".");
+            }
+            else if(j==19){
+                System.out.print("/");
+            }
+            else if(j==20){
+                System.out.print("|");
+            }
+            else if(j==24){
+                System.out.print("/");
+            }
+            else if(j==25){
+                System.out.print(".");
+            }
+            else if(j==26){
+                System.out.print(".");
+            }
+            else if(j==27){
+                System.out.print(".");
+            }
+            else if(j==28){
+                System.out.print(".");
+            }
+            else if(j==29){
+                System.out.print(".");
+            }
+            else if(j==30){
+                System.out.println("/");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static void NinthMethod(){
+        for(int j =1;j<=29;j++){
+            if(j==7){
+                System.out.print("\\");
+            }
+            else if(j==8){
+                System.out.print("-");
+            }
+            else if(j==9){
+                System.out.print(".");
+            }
+            else if(j==10){
+                System.out.print(".");
+            }
+            else if(j==11){
+                System.out.print(".");
+            }
+            else if(j==12){
+                System.out.print(".");
+            }
+            else if(j==13){
+                System.out.print("\\");
+            }
+            else if(j==16){
+                System.out.print("|");
+            }
+            else if(j==17){
+                System.out.print("\\");
+            }
+            else if(j==18){
+                System.out.print(".");
+            }
+            else if(j==19){
+                System.out.print("/");
+            }
+            else if(j==20){
+                System.out.print("|");
+            }
+            else if(j==23){
+                System.out.print("/");
+            }
+            else if(j==24){
+                System.out.print(".");
+            }
+            else if(j==25){
+                System.out.print(".");
+            }
+            else if(j==26){
+                System.out.print(".");
+            }
+            else if(j==27){
+                System.out.print(".");
+            }
+            else if(j==28){
+                System.out.print("-");
+            }
+            else if(j==29){
+                System.out.println("/");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static void TenthMethod(){
+        for(int j =1;j<=27;j++){
+            if(j==7){
+                System.out.print(" ");
+                System.out.print("\\");
+            }
+            else if(j==8){
+                System.out.print("-");
+            }
+            else if(j==9){
+                System.out.print("-");
+            }
+            else if(j==10){
+                System.out.print("-");
+            }
+            else if(j==11){
+                System.out.print("-");
+            }
+            else if(j==12){
+                System.out.print(".");
+            }
+            else if(j==13){
+                System.out.print("\\");
+            }
+
+            else if(j==16){
+                System.out.print("|");
+            }
+            else if(j==17){
+                System.out.print(".");
+            }
+            else if(j==18){
+                System.out.print("|");
+            }
+
+            else if(j==21){
+                System.out.print("/");
+            }
+            else if(j==22){
+                System.out.print(".");
+            }
+            else if(j==23){
+                System.out.print("-");
+            }
+            else if(j==24){
+                System.out.print("-");
+            }
+            else if(j==25){
+                System.out.print("-");
+            }
+            else if(j==26){
+                System.out.print("-");
+            }
+            else if(j==27){
+                System.out.println("/");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static void EleventhMethod(){
+        for(int j =1;j<=27;j++){
+            if(j==8){
+                System.out.print(" ");
+                System.out.print("\\");
+            }
+            else if(j==9){
+                System.out.print("-");
+            }
+            else if(j==10){
+                System.out.print("-");
+            }
+            else if(j==11){
+                System.out.print("-");
+            }
+            else if(j==12){
+                System.out.print("-");
+            }
+            else if(j==13){
+                System.out.print(".");
+            }
+            else if(j==14){
+                System.out.print("\\");
+            }
+
+            else if(j==16){
+                System.out.print("|");
+            }
+            else if(j==17){
+                System.out.print(".");
+            }
+            else if(j==18){
+                System.out.print("|");
+            }
+
+            else if(j==20){
+                System.out.print("/");
+            }
+            else if(j==21){
+                System.out.print(".");
+            }
+            else if(j==22){
+                System.out.print("-");
+            }
+            else if(j==23){
+                System.out.print("-");
+            }
+            else if(j==24){
+                System.out.print("-");
+            }
+            else if(j==25){
+                System.out.print("-");
+            }
+            else if(j==26){
+                System.out.println("/");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static void TwelfthMethod(){
+        for(int j =1;j<=27;j++){
+            if(j==9){
+
+                System.out.print("\\");
+            }
+            else if(j==10){
+                System.out.print("-");
+            }
+            else if(j==11){
+                System.out.print(".");
+            }
+            else if(j==12){
+                System.out.print(".");
+            }
+            else if(j==13){
+                System.out.print(".");
+            }
+            else if(j==14){
+                System.out.print(".");
+            }
+            else if(j==15){
+                System.out.print("\\");
+            }
+
+            else if(j==16){
+                System.out.print("|");
+            }
+            else if(j==17){
+                System.out.print(".");
+            }
+            else if(j==18){
+                System.out.print("|");
+            }
+
+            else if(j==19){
+                System.out.print("/");
+            }
+            else if(j==20){
+                System.out.print(".");
+            }
+            else if(j==21){
+                System.out.print(".");
+            }
+            else if(j==22){
+                System.out.print(".");
+            }
+            else if(j==23){
+                System.out.print(".");
+            }
+            else if(j==24){
+                System.out.print("-");
+            }
+            else if(j==25){
+                System.out.println("/");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println("           Selim AŞIK");
+    }
+}
+
